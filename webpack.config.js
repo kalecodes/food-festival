7c09940617c0acd9a946b1d6d023f05a702d0634
kalen510 <kalen.wiley@gmail.com>
2022-04-04 23:22:47 -0400

    optimize images

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,54 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+    // which file should be bundled
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/event.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
     },
+    // where the bundled file should be output to, and its name
+    output: {
+        filename: '[name].bundle.js',
+        path: __dirname + '/dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    // ability for webpack to understand jQuery
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
+        }),
+        // 
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to a report.html file in the dist folder
+            // we can also set this value to 'disable' to temporarily stop the reporting and automatic opening of this report in the browser
         })
     ],
     mode: `development`
