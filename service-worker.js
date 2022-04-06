5ccc9955b2fbb67a2082519b16499609e95ff196
kalen510 <kalen.wiley@gmail.com>
2022-04-05 22:25:08 -0400

    save

--- service-worker.js
+++ service-worker.js
@@ -46,4 +46,21 @@ self.addEventListener('activate', function (e) {
             )
         })
     )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            // if cache is available, respond with cache
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            // if there are no cache, try fetching request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
 })
